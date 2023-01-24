27336aba963e8100f174c06550548fc664180f6d
anoncoderin <113314047+anoncoderin@users.noreply.github.com>
2023-01-23 16:27:21 -0800

    initial commit

---
+++ .history/e2e/home.spec_20230123141317.ts
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test'
+
+var homePage = 'http://localhost:3000';
+var aboutPage = 'http://localhost:3000/about';
+var gradesPage = 'http://localhost:3000'/grades';
+
+test.beforeAll(async()=>{
+
+
+    console.log("Before tests")
+})
+
+test.afterAll(async()=>{
+    console.log('After tests');
+})
+
+test.describe('Head tag area',()=> {
+    test('Contains the Head tag and its content', async ({page})=>{
+        await page.goto(homePage);
+
+        await expect(page).toHaveTitle("Example#");
+        
+        const metaDescription = page.locator('meta[')
+    })
+})
